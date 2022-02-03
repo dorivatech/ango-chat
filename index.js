22be941585ad178440836bb82ba36a0960af510b
dorivatech <dorivatech@gmail.com>
2022-02-03 02:38:57 +0100

    Defini o pug como o view engine e adicionei o primeiro arquivo .pug

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+/** Defini o pug como o view engine */
+app.set('view engine', 'pug');
+
+/** Defini a pasta views como a pasta dos components de view */
 app.set('views', path.join(__dirname, 'views'));
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/', (req, res) => res.render('home'));
 
 app.listen(port, () => console.log(`App listening on port ${port} ✌❤`));
