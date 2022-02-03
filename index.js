a85a3e975b1ef74c914446a4db9ffeacd2f436d1
dorivatech <dorivatech@gmail.com>
2022-02-03 02:34:46 +0100

    Defini que a pasta das views será (views)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`App listening on port ${port} ✌❤`));
