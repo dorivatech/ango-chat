fd7ca11242b426cf00a70fb6f109394739e87113
dorivatech <dorivatech@gmail.com>
2022-02-03 03:05:26 +0100

    Defini a pasta public como a contedora dos arquivos estáticos

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-/** Defini o pug como o view engine */
+/** Defini o pug como o template engine */
 app.set('view engine', 'pug');
 
 /** Defini a pasta views como a pasta dos components de view */
 app.set('views', path.join(__dirname, 'views'));
+
+/** Defini a pasta public como a contedora dos arquivos estáticos */
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.render('home'));
 
