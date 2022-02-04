a5ace33983308ed068dfa1e271c7eadf34ba6036
dorivatech <dorivatech@gmail.com>
2022-02-04 19:47:55 +0100

    Adição do event/listener disconnect do socket

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => res.render('home'));
 
 io.on('connection', (socket) => {
-    console.log('A user connected', socket);
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    })
 });
 
 server.listen(port, () => console.log(`App listening on port ${port} ✌❤`));
