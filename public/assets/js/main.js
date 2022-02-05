b035a098db953daaabf600d21fc2a98805d5e83a
dorivatech <dorivatech@gmail.com>
2022-02-05 19:00:45 +0100

    Agora eu emito um evento do tipo 'stop typing' depois de enviar uma mensagem

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -23,7 +23,10 @@ Chat.getTextarea().addEventListener('focusout', function () {
     socket.emit('stop typing');
 });
 
-socket.on('chat message', message => Chat.newMessage(Math.round(Math.random()) == 0, message));
+socket.on('chat message', message => {
+    Chat.newMessage(Math.round(Math.random()) == 0, message);
+    socket.emit('stop typing');
+});
 socket.on('connected user', data => Chat.connectedDisconnectedUserListener('connected', data));
 socket.on('disconnected user', data => Chat.connectedDisconnectedUserListener('disconnected', data));
 socket.on('someone typing', data => Chat.someoneTyping(data));
